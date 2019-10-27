414b879958461246ceb9f619a34c5e885f59a752
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-10-27 17:32:28 +0900

    XXX

--- Program.fs
+++ Program.fs
@@ -1,8 +1,18 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FSharp.Data.Sql
 
 [<EntryPoint>]
 let main argv =
+    //
+    // Task
+    //   add(+) with details (-> due date can be added)
+    //   remove(+)
+    //   update
+    //   list (tasks)         
+    //   add duedate to task 
+    //   modify duedate in task 
+
     printfn "Hello World from F#!"
     0 // return an integer exit code
